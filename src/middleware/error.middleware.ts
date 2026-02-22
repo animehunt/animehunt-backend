a6fc879007a8df724e1a92ff3eb930fa15b09af2
animehunt <animehunt715@gmail.com>
2026-02-23 01:47:58 +0530

    Create error.middleware.ts

---
+++ src/middleware/error.middleware.ts
@@ -0,0 +1,20 @@
+import { Request, Response, NextFunction } from "express";
+
+export const errorHandler = (
+  err: any,
+  _req: Request,
+  res: Response,
+  _next: NextFunction
+) => {
+  console.error("🔥 ERROR:", err);
+
+  const statusCode = err.statusCode || 500;
+
+  res.status(statusCode).json({
+    success: false,
+    message:
+      process.env.NODE_ENV === "production"
+        ? "Internal Server Error"
+        : err.message,
+  });
+};
