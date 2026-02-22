73a96c665fa90b7da8c5a5d09a25b801cb82ef1b
animehunt <animehunt715@gmail.com>
2026-02-23 00:31:14 +0530

    Update auth.middleware.ts

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -1,28 +1,20 @@
 import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
 
-export const verifyAdmin = (
+export const authGuard = (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  const auth = req.headers.authorization;
+  const header = req.headers.authorization;
+  if (!header) return res.status(401).json({ message: "No token" });
 
-  if (!auth) {
-    return res.status(401).json({ message: "No token" });
-  }
-
-  const token = auth.split(" ")[1];
+  const token = header.split(" ")[1];
 
   try {
-    const decoded = jwt.verify(
-      token,
-      process.env.JWT_SECRET as string
-    );
-
-    (req as any).user = decoded;
+    jwt.verify(token, process.env.JWT_SECRET!);
     next();
   } catch {
-    return res.status(403).json({ message: "Invalid token" });
+    res.status(401).json({ message: "Invalid token" });
   }
 };
