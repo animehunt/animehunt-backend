816e9eaa6ad70989ddbd9111dd2778bdfd16ae68
animehunt <animehunt715@gmail.com>
2026-02-23 13:33:08 +0530

    Update system.model.ts

--- src/models/system.model.ts
+++ src/models/system.model.ts
@@ -1,26 +1,58 @@
 import mongoose, { Schema, Document } from "mongoose";
 
 export interface ISystem extends Document {
-  status: string;
-  versions: { name: string; createdAt: Date }[];
-  backups: { name: string; createdAt: Date }[];
+  status: "live" | "frozen" | "shutdown";
+  config: any;
+  killed: boolean;
+
+  versions: {
+    name: string;
+    createdAt: Date;
+  }[];
+
+  backups: {
+    name: string;
+    createdAt: Date;
+  }[];
 }
 
 const SystemSchema = new Schema<ISystem>(
   {
-    status: { type: String, default: "live" }, // live, frozen, shutdown
+    status: {
+      type: String,
+      enum: ["live", "frozen", "shutdown"],
+      default: "live",
+    },
+
+    config: {
+      type: Schema.Types.Mixed,
+      default: {},
+    },
+
+    killed: {
+      type: Boolean,
+      default: false,
+    },
+
     versions: [
       {
         name: String,
-        createdAt: { type: Date, default: Date.now }
-      }
+        createdAt: {
+          type: Date,
+          default: Date.now,
+        },
+      },
     ],
+
     backups: [
       {
         name: String,
-        createdAt: { type: Date, default: Date.now }
-      }
-    ]
+        createdAt: {
+          type: Date,
+          default: Date.now,
+        },
+      },
+    ],
   },
   { timestamps: true }
 );
