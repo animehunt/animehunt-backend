03d31e539addc85dc2c8a79b859b7279f4bdea1a
animehunt <animehunt715@gmail.com>
2026-02-28 03:41:48 +0530

    Update auth.routes.ts

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,9 +1,8 @@
 import express from "express";
-import { login, verify } from "../controllers/auth.controller";
+import { login } from "../controllers/auth.controller";
 
 const router = express.Router();
 
 router.post("/auth/login", login);
-router.post("/auth/verify", verify);
 
 export default router;
