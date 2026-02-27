6d8266b8aef48895ca9e61f8421e7f029ce32968
animehunt <animehunt715@gmail.com>
2026-02-28 02:18:24 +0530

    Delete src/routes/Admin.ts

--- src/routes/Admin.ts
+++
@@ -1,9 +0,0 @@
-app.get("/create-admin-now", async (req, res) => {
-
-  await Admin.create({
-    username: "anime_moderator_007",
-    password: "$2a$12$1RdWqkMMG4j/haO5CROyqeh37cXvV6cYGXqY0YcoKFDpccBQFJHle"
-  });
-
-  res.send("Admin Created Successfully ✅");
-});
