5f9ec734114917e31615b71cb1fab0e9443bc0b6
animehunt <animehunt715@gmail.com>
2026-02-28 23:46:37 +0530

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -10,6 +10,7 @@ import analyticsRoutes from "./routes/analytics.routes";
 import animeRoutes from "./routes/anime.routes";
 import bannerRoutes from "./routes/banner.routes";
 import categoryRoutes from "./routes/category.routes";
+import deployRoutes from "./routes/deploy.routes";
 
 dotenv.config();
 connectDB();
@@ -27,6 +28,7 @@ app.use("/api/admin/analytics", analyticsRoutes
 app.use("/api/admin/anime", animeRoutes);
 app.use("/api/admin/banners", bannerRoutes);
 app.use("/api/admin/categories", categoryRoutes);
+app.use("/api/admin/deploy", deployRoutes);
 
 app.get("/", (_req, res) => {
   res.send("AnimeHunt Ads API Running 🚀");
