a4d3135bf45e028992398def83ea879057e1a6c3
animehunt <animehunt715@gmail.com>
2026-03-01 14:42:57 +0530

    Delete src directory

--- src/routes/securityStats.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { getSecurityStats } from "../controllers/securityStats.controller";
-
-const router = Router();
-
-router.get("/stats", getSecurityStats);
-
-export default router;
--- src/routes/seo.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { getSEO, saveSEO } from "../controllers/seo.controller";
-
-const router = Router();
-
-router.get("/", getSEO);
-router.post("/", saveSEO);
-
-export default router;
