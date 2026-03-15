c3bcee03d4cb9304ef11037af289598a4067aa6f
animehunt <animehunt715@gmail.com>
2026-03-15 20:56:23 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -12,3 +12,18 @@ app.get('/', (c) => {
 app.route('/api/admin', adminAuth)
 
 export default app
+import { Hono } from 'hono'
+
+import auth from './routes/auth.js'
+import dashboard from './routes/dashboard.js'
+import system from './routes/system.js'
+
+const app = new Hono()
+
+app.get('/', c => c.json({status:"AnimeHunt API Running"}))
+
+app.route('/api/admin', auth)
+app.route('/api/admin', dashboard)
+app.route('/api/admin/system', system)
+
+export default app
