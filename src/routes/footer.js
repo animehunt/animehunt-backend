6185d34fcd678e99bdba464df98b525d32a9871c
animehunt <animehunt715@gmail.com>
2026-07-20 01:37:40 +0530

    Update footer.js

--- src/routes/footer.js
+++ src/routes/footer.js
@@ -168,11 +168,11 @@ function format(r) {
 ================================================ */
 
 function syncToReplicas(env, row) {
-  if (env.TURSO_URL && env.TURSO_AUTH_TOKEN) {
-    fetch(`${env.TURSO_URL}/v2/pipeline`, {
+  if (env.TURSO_REPLICA_URL && env.TURSO_REPLICA_AUTH_TOKEN) {
+    fetch(`${env.TURSO_REPLICA_URL}/v2/pipeline`, {
       method: "POST",
       headers: {
-        "Authorization": `Bearer ${env.TURSO_AUTH_TOKEN}`,
+        "Authorization": `Bearer ${env.TURSO_REPLICA_AUTH_TOKEN}`,
         "Content-Type":  "application/json"
       },
       body: JSON.stringify({
